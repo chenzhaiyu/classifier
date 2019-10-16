f801fc2fb5cfafc60d6adfba4ee6d97630216621
czy <contact@chenzhaiyu.com>
2019-10-16 17:27:41 +0200

    Update mlc.py

--- mlc.py
+++ mlc.py
@@ -5,9 +5,11 @@ Multi-spectral Classification on Red & NIR Image with Maximum-likelihood Classif
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 IMAGE_SIZE = 20
 EPSILON = 1e-6
+CLASS_IDS = [1, 2, 3]
 
 
 class MultiSpectralImage:
@@ -21,9 +23,19 @@ class MultiSpectralImage:
 
     def nvdi(self):
         """
-        Compute NDVI values given RED and NIR image
+        Compute NDVI values given RED and NIR images
         """
         return (self.nir - self.red) / (self.nir + self.red + EPSILON)
+
+    def find_class(self, class_id):
+        """
+        Find pixels on RED and NIR values corresponding to specific class
+        class_id: 0 is non-training data, 1 is vegetation, 2 is bare ground, 3 is water
+        """
+        is_class = (self.label == class_id)
+        vred = self.red[is_class]
+        vnir = self.nir[is_class]
+        return vred, vnir
 
     @staticmethod
     def plot_hist(ndvi):
@@ -40,11 +52,18 @@ class MaximumLikelihoodClassifier:
     """
     Maximum-likelihood Classifier
     """
-    def __init__(self, msimage):
-        self.msimge = msimage
+    def __init__(self, training_samples):
+        self.training_samples = training_samples
 
-    def learn(self):
-        pass
+    def fit_samples(self, class_id):
+        """
+        Get mean and standard deviation using MLE
+        """
+        # TODO: may be wrong to simply use norm.fit
+        mean_red, std_red = norm.fit(training_samples[class_id - 1][0])
+        mean_nir, std_nir = norm.fit(training_samples[class_id - 1][1])
+        return mean_red, std_red, mean_nir, std_nir
+    # TODO: get the formula, link to class
 
 
 def load_data(csv_path):
@@ -60,7 +79,19 @@ def load_data(csv_path):
 
 
 if __name__ == '__main__':
+    # Load data
     msimage = MultiSpectralImage(load_data("data/RED.csv"), load_data("data/NIR.csv"), load_data("data/label.csv"))
+
+    # Compute NDVI
     ndvi = msimage.nvdi()
     msimage.plot_hist(ndvi)
 
+    # Prepare training samples
+    training_samples = []
+    for class_id in CLASS_IDS:
+        training_samples.append(msimage.find_class(class_id))
+
+    # Classify
+    mlclassifier = MaximumLikelihoodClassifier(training_samples)
+    for class_id in CLASS_IDS:
+        mlclassifier.fit_samples(class_id)
